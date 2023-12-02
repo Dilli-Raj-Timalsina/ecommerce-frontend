6dc6d192a914ae5a4cb3f394cd69733f8151ddfc
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-12-02 08:33:17 +0545

    stripe package installed

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,72 +1,81 @@
-'use client';
+"use client";
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from "react";
 import { useModalContext } from "./ModalContext";
 
 interface ICartContext {
-	cart: any[]
-	addItemToCart: ({ id }: { id: string; }) => Promise<void>
-	deleteItemFromCart: ({ id }: { id: string; }) => Promise<void>
+    cart: any[];
+    addItemToCart: ({ id }: { id: string }) => Promise<void>;
+    deleteItemFromCart: ({ id }: { id: string }) => Promise<void>;
 }
 
 const CartContext = createContext<ICartContext | undefined>(undefined);
 
 export function CartProvider({ children }: { children: React.ReactNode }) {
-	const { showModal } = useModalContext()
-	const [cart, setCart] = useState<any[]>([]);
+    const { showModal } = useModalContext();
+    const [cart, setCart] = useState<any[]>([]);
 
-	const setCartToState = () => {
-		const cartDataString = localStorage.getItem('cart');
-		const cartData = cartDataString !== null ? JSON.parse(cartDataString) : { cartItems: [] }
-		setCart(cartData.cartItems)
-	}
+    const setCartToState = () => {
+        const cartDataString = localStorage.getItem("cart");
+        const cartData =
+            cartDataString !== null
+                ? JSON.parse(cartDataString)
+                : { cartItems: [] };
+        setCart(cartData.cartItems);
+    };
 
-	const addItemToCart = async (id: { id: string }) => {
-		let newCartItems;
-		if (!cart.includes(id)) {
-			newCartItems = [...(cart || []), id]
-		} else {
-			newCartItems = [...(cart)]
-		}
+    const addItemToCart = async (id: { id: string }) => {
+        let newCartItems;
+        if (!cart.includes(id)) {
+            newCartItems = [...(cart || []), id];
+        } else {
+            newCartItems = [...cart];
+        }
 
-		showModal("Item successfully added to cart", "Info");
+        showModal("Item successfully added to cart", "Info");
 
-		localStorage.setItem("cart", JSON.stringify({ cartItems: newCartItems }));
-		setCartToState()
-	}
+        localStorage.setItem(
+            "cart",
+            JSON.stringify({ cartItems: newCartItems })
+        );
+        setCartToState();
+    };
 
-	const deleteItemFromCart = async (id: { id: string }) => {
-		let newCartItems = cart?.filter((i) => i !== id);
+    const deleteItemFromCart = async (id: { id: string }) => {
+        let newCartItems = cart?.filter((i) => i !== id);
 
-		showModal("Item successfully deleted from cart", "Info");
-		localStorage.setItem("cart", JSON.stringify({ cartItems: newCartItems }));
-		setCartToState()
-	}
+        showModal("Item successfully deleted from cart", "Info");
+        localStorage.setItem(
+            "cart",
+            JSON.stringify({ cartItems: newCartItems })
+        );
+        setCartToState();
+    };
 
-	useEffect(() => {
-		setCartToState()
-	}, [])
+    useEffect(() => {
+        setCartToState();
+    }, []);
 
-	return (
-		<CartContext.Provider
-			value={{
-				cart,
-				addItemToCart,
-				deleteItemFromCart
-			}}
-		>
-			{children}
-		</ CartContext.Provider>
-	);
+    return (
+        <CartContext.Provider
+            value={{
+                cart,
+                addItemToCart,
+                deleteItemFromCart,
+            }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
 }
 
 export const useCartContext = () => {
-	const context = useContext(CartContext)
-	if (context === undefined) {
-		throw new Error("useCartContext must be within CartContext")
-	}
+    const context = useContext(CartContext);
+    if (context === undefined) {
+        throw new Error("useCartContext must be within CartContext");
+    }
 
-	return context
-}
+    return context;
+};
 
-export default CartContext
+export default CartContext;
