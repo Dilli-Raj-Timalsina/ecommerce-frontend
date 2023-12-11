bf5271f2f6588c478a23dc95aaf7c1b200a501c8
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-12-11 17:18:45 +0545

    admin route protected

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import DashBoardSideBar from "@/components/Admin/DashBoardSideBar";
-import { useState } from "react";
+import { useAuthContext } from "@/context/AuthContext";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 import { useContext } from "react";
 import { useRouter } from "next/navigation";
@@ -19,6 +20,7 @@ export const CategoryContext = createContext<CategoryContextType>({
 export default function Home({ children }: { children: React.ReactNode }) {
     const [productState, setProductState] = useState(false);
     const [category, setCategory] = useState("clothes");
+    const { user } = useAuthContext();
     const router = useRouter();
 
     const handleCategoryChange = (
@@ -27,44 +29,52 @@ export default function Home({ children }: { children: React.ReactNode }) {
         console.log(event.target.value);
         setCategory(event.target.value);
     };
+    useEffect(() => {
+        if (user && user.id != "40") {
+            router.push("/login");
+        }
+    }, [user]);
     return (
-        <div className="">
-            <div className="flex justify-between items-end pr-2  ">
-                <h1
-                    className="text-gray-600 font-bold px-3 mt-6 ml-2 mb-3 text-2xl cursor-pointer "
-                    onClick={() => {
-                        router.push("/admin/dashboard");
-                    }}
-                >
-                    USHOPIE
-                </h1>
+        user &&
+        user.id && (
+            <div className="">
+                <div className="flex justify-between items-end pr-2  ">
+                    <h1
+                        className="text-gray-600 font-bold px-3 mt-6 ml-2 mb-3 text-2xl cursor-pointer "
+                        onClick={() => {
+                            router.push("/");
+                        }}
+                    >
+                        USHOPIE
+                    </h1>
 
-                <select
-                    id="category"
-                    name="category"
-                    value={category}
-                    onChange={handleCategoryChange}
-                    className="w-72 h-fit px-3 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:drop-shadow-md"
-                    required
-                >
-                    {categories.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </select>
+                    <select
+                        id="category"
+                        name="category"
+                        value={category}
+                        onChange={handleCategoryChange}
+                        className="w-72 h-fit px-3 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:drop-shadow-md"
+                        required
+                    >
+                        {categories.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700" />
+
+                <section className="flex md:flex-row flex-col">
+                    <DashBoardSideBar
+                        setProductState={setProductState}
+                        productState={productState}
+                    ></DashBoardSideBar>
+                    <CategoryContext.Provider value={{ category, setCategory }}>
+                        {children}
+                    </CategoryContext.Provider>
+                </section>
             </div>
-            <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700" />
-
-            <section className="flex md:flex-row flex-col">
-                <DashBoardSideBar
-                    setProductState={setProductState}
-                    productState={productState}
-                ></DashBoardSideBar>
-                <CategoryContext.Provider value={{ category, setCategory }}>
-                    {children}
-                </CategoryContext.Provider>
-            </section>
-        </div>
+        )
     );
 }
